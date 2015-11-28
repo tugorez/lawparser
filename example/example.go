de0a0679aae88bcb808dc65350208298dbc2ac5b
tugorez <tugorez@gmail.com>
2015-11-27 23:42:34 -0600

    corregí los datos del test (estaba como nombre un doc del df y era de bj

--- example/example.go
+++ example/example.go
@@ -9,13 +9,13 @@ import (
 func main() {
 	src, err := ioutil.ReadFile("bj.txt")
 	p, err := lawparser.Parse(
-		"ley de tránsito del distrito federal", //nombre
-		"México",           //pais
-		"Distrito Federal", //estado
-		"all",              //municipio
-		"estatal",          //orden federal,estatal o municipal
-		"ley",              //ley o reglamento o ...
-		"tránsito",         //a que topico de interes comun pertenece
+		"Reglamento de tránsito de Benito Juarez", //nombre
+		"México",        //pais
+		"Quintana Roo",  //estado
+		"Benito Juárez", //municipio
+		"estatal",       //orden federal,estatal o municipal
+		"reglamento",    //ley o reglamento o ...
+		"tránsito",      //a que topico de interes comun pertenece
 		"http://cancun.gob.mx/transparencia/files/2011/12/Reg_Transitoparael-Municipiode-BenitoJuarez_QRoo_30dic14.pdf", //de donde viene este doc
 		src,
 	)
